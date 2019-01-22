de4c522a444339ec347d76393de1c7fedf0a7c62
Mike Godley <ubuntu@ip-172-31-31-75.eu-west-1.compute.internal>
2019-01-22 23:59:12 +0000

    Updated environment

--- dev.sh
+++ dev.sh
@@ -3,38 +3,48 @@ set -x
 
 # Update the OS
 sudo apt-get update
-sudo apt-get install -y
+sudo apt-get upgrade -y
+
+# Install utilities
+suso apt-get install make -y
 
 # Install Emacs
 sudo apt-get install emacs25 -y
-
-# Install globally eslint, babel-eslint, eslint-plugin-react
-# sudo npm install -g eslint babel-eslint eslint-plugin-react
+git clone https://github.com/michaelgodley/emacs.git
 
 # Setup soft links to config files
 dir="$(pwd)"
 cd $HOME
-ln -sb $dir/.emacs .
-ln -sb $dir/.emacs.d .
+ln -sb $dir/emacs/.emacs .
+ln -sb $dir/emacs/.emacs.d .
 ln -sb $dir/.screenrc .
-ln -sb $dir/.eslintrc .
 
-# Install Docker and Docker Compose
-sudo apt-get remove -y docker docker-engine docker.io docker-ce
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL http://download.docker.com/linux/ubuntu/gpg |sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo groupadd docker
+# Install Docker 
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x ./get-docker.sh
+sudo ./get-docker.sh
 sudo usermod -aG docker $USER
-sudo systemctl enable docker
 
-dockerComposeVersion="${dockerComposeVersion:-1.21.2}"
+# Install Docker Compose
+dockerComposeVersion=$DOCKERCOMPOSEVERSION
 sudo curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Node JS
+# Install Node JS
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# set git and npm variables
+git config --global user.name $GIT_USER_NAME
+git config --global user.email $GIT_USER_EMAIL
+npm set init.author.name $NPM_INIT_AUTHOR_NAME
+npm set init.author.email $NPM_INIT_AUTHOR_EMAIL
+npm set init.license $NPM_INIT_LICENSE
+npm set init.version $NPM_INIT_VERSION
+
+# Setup a Local NPM Registry
+git clone http://github.com/michaelgodley/npmlocalregistry.git
+cd npmlocalregistry
+docker-compose up -d
+cd ..
+
--- prod.sh
+++ prod.sh
@@ -3,21 +3,17 @@ set -x
 
 # Update the OS
 sudo apt-get update
-sudo apt-get install -y
+sudo apt-get upgrade -y
 
-# Install Docker and Docker Compose
-sudo apt-get remove -y docker docker-engine docker.io docker-ce
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL http://download.docker.com/linux/ubuntu/gpg |sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
-sudo groupadd docker
+# Install Docker 
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x ./get-docker.sh
+sudo ./get-docker.sh
 sudo usermod -aG docker $USER
-sudo systemctl enable docker
 
-sudo rm /usr/local/bin/docker-compose 
-dockerComposeVersion="${dockerComposeVersion:-1.18.0}"
+# Install Docker Compose
+dockerComposeVersion=$DOCKERCOMPOSEVERSION
 sudo curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+
