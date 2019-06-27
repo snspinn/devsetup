663037a914117fc311ac850270e73bf23dfbbccb
Mike Godley <michaelgodley@gmail.com>
2019-06-27 22:18:13 +0000

    updated versions

--- dev.sh
+++ dev.sh
@@ -9,7 +9,7 @@ sudo apt-get upgrade -y
 suso apt-get install make -y
 
 # Install Emacs
-sudo apt-get install emacs26 -y
+sudo apt-get install emacs25 -y
 git clone https://github.com/michaelgodley/emacs.git
 
 # Setup soft links to config files
@@ -30,7 +30,8 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
-DOCKER_COMPOSE_VERSION="${dockerComposeVersion:-1.24.0}"
+dockerComposeVersion="${dockerComposeVersion:-1.24.0}"
+# dockerComposeVersion=$DOCKERCOMPOSEVERSION
 sudo curl -L https://github.com/docker/compose/releases/download/$dockerComposeVersion/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
@@ -39,12 +40,12 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # set git and npm variables
-git config --global user.name $GIT_USER_NAME
-git config --global user.email $GIT_USER_EMAIL
-npm set init.author.name $NPM_INIT_AUTHOR_NAME
-npm set init.author.email $NPM_INIT_AUTHOR_EMAIL
-npm set init.license $NPM_INIT_LICENSE
-npm set init.version $NPM_INIT_VERSION
+#git config --global user.name $GIT_USER_NAME
+#git config --global user.email $GIT_USER_EMAIL
+#npm set init.author.name $NPM_INIT_AUTHOR_NAME
+#npm set init.author.email $NPM_INIT_AUTHOR_EMAIL
+#npm set init.license $NPM_INIT_LICENSE
+#npm set init.version $NPM_INIT_VERSION
 
 # Setup a Local NPM Registry
 git clone http://github.com/michaelgodley/npmlocalregistry.git
