746e96497b149e4ce0c6029fd4e6873687ef2889
Mike Godley <michaelgodley@gmail.com>
2020-07-02 20:47:30 +0000

    Updated Node script for React

--- scripts/node.sh
+++ scripts/node.sh
@@ -7,3 +7,4 @@
 # Install Node JS
 curl -sL https://deb.nodesource.com/setup_$NODE_VERSION | sudo -E bash -
 aptinstall nodejs
+echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
